d423cd10bb36a65510af8e3aed27cb310b08d73a
Ivar Stefansson <ivar.stefansson@uib.no>
2026-01-26 12:40:17 +0000

    parameter values

--- material_parameters.py
+++ material_parameters.py
@@ -42,7 +42,8 @@ granodiorite_values = {
     "permeability": 5e-15,  # [2], p. 1
     "fracture_gap": 0e-4,  # TODO: Find a reference and a value
     "residual_aperture": 1e-3,  # TODO: Find a reference and a value
-    "well_radius": 1e-1,  # TODO: Find a reference and a value. Not too far off if LineID can be trusted.
+    "well_radius": 2e-1,  # TODO: Find a reference and a value. Not too far off if
+    # LineID can be trusted. Increase to 0.2 m, to avoid pressure drop between wells?
     "normal_permeability": 1e-8,  # TODO: Reconsider model.
     "fracture_normal_stiffness": 1e9,  # TODO: Find a reference and a value
     "maximum_elastic_fracture_opening": 0e-3,  # TODO: Find a reference and a value
